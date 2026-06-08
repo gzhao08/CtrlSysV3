c0c948b37087f1336cf80b17c19373bdc5ea32ab
Gordon Zhao <gordonzhao00@gmail.com>
2026-06-08 16:22:52 -0700

    removed enable since same as reset

--- source/hdl/stopwatch_64.sv
+++ source/hdl/stopwatch_64.sv
@@ -8,14 +8,13 @@ a 64 bit counter incrementing every 20ns (50MHz clock) would take > 10000 years 
 module timekeep (
     input               clk,
     input               rst,
-    input               enable,
     output logic [63:0] timestamp_counter
 );	
 
 always_ff @(posedge clk) begin
     if (rst)
         timestamp_counter <= 64'd0;
-    else if (enable)
+    else
         timestamp_counter <= timestamp_counter + 1;
 end
 
