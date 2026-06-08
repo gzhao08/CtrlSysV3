ac4024994c649802d6ee37a84dceaca71bc9758e
Gordon Zhao <gordonzhao00@gmail.com>
2026-06-08 16:00:44 -0700

    refactored setup to use paths relative to root

--- tests/compilation/run.py
+++ tests/compilation/run.py
@@ -1,21 +1,5 @@
-import subprocess
-from pathlib import Path
-import os
+import compile
+import setup
 
-work_dir = "source/hdl"
-
-os.chdir(work_dir)
-
-# Get file names in the current directory
-file_names = [f.name for f in Path('.').iterdir() if f.is_file()]
-# print(file_names)
-
-prefix = "config"
-
-reordered = sorted(file_names, key=lambda word: not word.startswith(prefix))
-
-command = ["iverilog", "-g2012", "-tnull"] + reordered
-
-print(" ".join(command))
-
-subprocess.run(command)
+setup.setup()
+compile.compile()
